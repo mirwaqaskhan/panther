c3a5ccbd981ddbb8617d239a64a19cf3d97ef6ea
mirwaqaskhan <waqas.khan@zeptosystems.com>
2018-11-14 11:57:19 +0500

    Empty Base App

---
+++ app/src/main/java/com/byteful/panther/PlaylistsOverview.kt
@@ -0,0 +1,12 @@
+package com.byteful.panther
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaylistsOverview : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlists_overview)
+    }
+}
