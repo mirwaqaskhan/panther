7dd5808b38d7ad4fbc1ac1326d00352a3ac0737b
EhtishamShami <theehtishamshami@live.com>
2018-11-14 19:34:02 +0500

    First Push

--- app/src/main/java/com/byteful/panther/PlaylistsOverview.kt
+++ app/src/main/java/com/byteful/panther/PlaylistsOverview.kt
@@ -8,5 +8,6 @@ class PlaylistsOverview : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlists_overview)
+        
     }
 }
